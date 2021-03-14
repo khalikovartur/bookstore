fb936c36f66fcfa0f2624338e986c4077bc19282
Artur <khalikart@gmail.com>
2021-03-14 19:57:39 +0500

    images uploads ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -165,3 +165,6 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
 DEFAULT_FROM_EMAIL = 'admin@djangoprojectbookstore.com'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
