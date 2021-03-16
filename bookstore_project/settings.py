6971525704287a5f5c8a37adb1f04a5237091142
Artur <khalikart@gmail.com>
2021-03-16 10:45:02 +0500

    added test pay sistem ch14

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'books.apps.BooksConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -168,3 +169,6 @@ DEFAULT_FROM_EMAIL = 'admin@djangoprojectbookstore.com'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
+STRIPE_TEST_SECRET_KEY=os.environ.get('STRIPE_TEST_SECRET_KEY')
