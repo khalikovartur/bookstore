0a3ad0eab96e3280e19e136fc5579d1c6a4b43e9
khalikovartur <72015866+khalikovartur@users.noreply.github.com>
2021-03-25 19:21:08 +0500

    Update settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -164,7 +164,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
